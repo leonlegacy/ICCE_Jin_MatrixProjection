f10102ccad2f43bfefbf62899aaafdac32990b0b
Leon <bsexp357079@gmail.com>
2022-02-12 17:44:41 +0800

    GameManager Created

---
+++ Assets/gameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public SkinnedMeshRenderer handR, handL;
+    public bool isShowHands = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            isShowHands = !isShowHands;
+            setMeshVisible(isShowHands);
+        }
+    }
+
+    public void setMeshVisible(bool flag)
+    {
+        handL.enabled = flag;
+        handR.enabled = flag;
+    }
+}
